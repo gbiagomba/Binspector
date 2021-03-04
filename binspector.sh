e71f13fdc7d25114f3ae8c26e529659c737fa103
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2021-03-04 12:49:16 -0500

    added new features

--- binspector.sh
+++ binspector.sh
@@ -15,15 +15,14 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Declaring variables
-pth=$(pwd)
-TodaysDAY=$(date +%m-%d)
-TodaysYEAR=$(date +%Y)
-wrkpth="$pth/$TodaysYEAR/$TodaysDAY"
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+pth=$PWD
+wrkpth="$PWD/Binspector"
 wrktmp=$(mktemp -d)
 bin=$1
 
 # Setting Envrionment
-mkdir -p  $wrkpth/PEFrame/ $wrkpth/Zzuf/ $wrkpth/Valgrind/
+mkdir -p  $wrkpth/PEFrame/ $wrkpth/Zzuf/ $wrkpth/Valgrind/ $wrkpth/Binwalk/ $wrkpth/cve-bin-tool/
 
 # Moving back to original workspace & loading logo
 cd $pth
@@ -59,35 +58,51 @@ echo
 echo "--------------------------------------------------"
 echo "Checking for banned strings"
 echo "--------------------------------------------------"
-peframe $bin | tee $wrkpth/PEFrame/$prj_name-peframe_output.txt
-echo | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-echo | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-echo "--------------------------------------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-echo "Strings" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-echo "--------------------------------------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-peframe -s $bin | tee -a $wrkpth/PEFrame/$prj_name-peframe_output.txt
-peframe -j $bin | tee $wrkpth/PEFrame/$prj_name-peframe_output.json
+peframe $bin | tee $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt 2> /dev/null
+echo | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt
+echo | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt
+echo "--------------------------------------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt
+echo "Strings" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt
+echo "--------------------------------------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt
+peframe -s $bin | tee -a $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt 2> /dev/null
+peframe -j $bin | tee $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.json 2> /dev/null
 
 for str in $(cat /opt/Binspector/sdl_banned_funct.list); do
-    if [ "`cat $wrkpth/PEFrame/$prj_name-peframe_output.txt | grep -o $str`" == "$str" ]; then
-        echo "--------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
-        echo "Checking for $str" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
-        echo "--------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
-        cat $wrkpth/PEFrame/$prj_name-peframe_output.txt | grep $str | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
-        echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
-        # echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
+    if [ "`cat $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt | grep -o $str`" == "$str" ]; then
+        echo "--------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
+        echo "Checking for $str" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
+        echo "--------------------------------------------------" | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
+        cat $wrkpth/PEFrame/$prj_name-peframe_output-$current_time.txt | grep $str | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
+        echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
+        # echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
     fi
 done
 
 # Troubleshoot if statement below
-if [ ! -z $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt ]; then
-    echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
+if [ -z $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt ]; then
+    echo | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
     echo "For more information on why you shouldn't use the aforementioned functions, see links below:
     https://security.web.cern.ch/security/recommendations/en/codetools/c.shtml
     https://github.com/intel/safestringlib/wiki/SDL-List-of-Banned-Functions
     https://docs.microsoft.com/en-us/previous-versions/bb288454(v=msdn.10)?redirectedfrom=MSDN
-    " | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct.txt
+    " | tee -a $wrkpth/PEFrame/$prj_name-sdl_banned_funct-$current_time.txt
 fi
+echo 
+
+# Placeholder
+echo "--------------------------------------------------"
+echo "Running binwalk"
+echo "--------------------------------------------------"
+cd $wrkpth/Binwalk/
+binwalk -e $bin | tee $wrkpth/Binwalk/$prj_name-binwalk_output-$current_time.txt 2> /dev/null
+cd $pth
+echo 
+
+# Placeholder
+echo "--------------------------------------------------"
+echo "Running cve-bin-tool"
+cd $wrkpth/cve-bin-tool/
+cve-bin-tool -i $bin -o $wrkpth/cve-bin-tool/$prj_name-cve-bin-tool_output-$current_time.txt -c 4 2> /dev/null
 echo 
 
 # Fuzzing executable binary
@@ -96,10 +111,3 @@ echo "Fuzzing executable binary"
 echo "--------------------------------------------------"
 # Command will go here
 echo
-
-# Placeholder
-echo "--------------------------------------------------"
-echo "Placeholder"
-echo "--------------------------------------------------"
-# Command will go here
-echo 
--- install.sh
+++ install.sh
@@ -16,30 +16,53 @@ if [ "$OS_CHK" != "debian" ]; then
     exit
 fi
 
-if [ ! -x /usr/local/bin/peframe ] || [ ! -x /usr/bin/peframe ]; then
+# Installing cve-bin-tool
+if [ ! hash cve-bin-tool ];
+    pip3 install cve-bin-tool
+fi
+
+# Installing peframe
+if [ ! hash peframe ]; then
     apt install peframe -y
-    if [ ! -x /usr/local/bin/peframe ] || [ ! -x /usr/bin/peframe ]; then
+    if [ ! hash peframe ]; then
         cd /opt/
         git clone https://github.com/guelfoweb/peframe
         cd peframe/
+        pip3 install -r requirements.txt
         python3 setup.py install
         bash.install
     fi
 fi
 
-if [ ! -x /usr/bin/zzuf ]; then
+# Installing binwalk
+if [ ! hash binwalk ]
+    apt install binwalk -y
+    if [ ! hash binwalk ]
+        git clone https://github.com/ReFirmLabs/binwalk
+        cd binwalk
+        python3 setup.py install
+    fi
+fi
+
+# Installing zzuf
+if [ ! hash /usr/bin/zzuf ]; then
     apt install zzuf -y
 fi
 
-if [ ! -x /usr/bin/valgrind ]; then
+# Installing valgrind
+if [ ! hash /usr/bin/valgrind ]; then
     apt install valgrind -y
+    if [ ! hash /usr/bin/valgrind ]; then
+        wget -q https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2
+        cd valgrind-3.16.1
+        ./configure
+        make
+        make install
+    fi
 fi
 
 # Downloading the Vulners Nmap Script
-cd /opt/
-git clone https://github.com/gbiagomba/Binspector
-cd /usr/bin/
-ln -s /opt/Binspector/binspector.sh binspector
+ln -s /opt/Binspector/binspector.sh /usr/bin/binspector
 
 # Done
 echo finished!
