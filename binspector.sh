e479780389e1b2584878f9e9b83c1918de2804d9
D4rth R3v4n <gbiagomba@users.noreply.github.com>
2021-07-08 19:10:18 -0400

    Update binspector.sh

--- binspector.sh
+++ binspector.sh
@@ -126,7 +126,7 @@ echo "--------------------------------------------------"
 echo "Running cve-bin-tool"
 echo "--------------------------------------------------"
 cd $wrkpth/cve-bin-tool/
-timeout 900 cve-bin-tool -x -f csv -i $pth/$bin -o $wrkpth/cve-bin-tool/$prj_name-cve-bin-tool_output-$current_time.csv -c 4 -u now 2> /dev/null | tee -a  $wrkpth/cve-bin-tool/$prj_name-cve-bin-tool_output-$current_time.log
+timeout 900 cve-bin-tool -x -f csv -o $wrkpth/cve-bin-tool/$prj_name-cve-bin-tool_output-$current_time.csv -c 4 -u now $pth/$bin 2> /dev/null | tee -a  $wrkpth/cve-bin-tool/$prj_name-cve-bin-tool_output-$current_time.log
 cd $pth
 echo 
 
